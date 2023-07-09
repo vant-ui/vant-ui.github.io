6718726d04a8c0a568ac3c4a11fe9cbefa060e72
chenjiahan <jait.chen@foxmail.com>
2023-07-09 08:32:46 +0000

    Deploying to main from @ youzan/vant@871d1ed3a19d6c7d6eaa5378d86585d646cb18b7 🚀

---
+++ vant/assets/index-990dedbe.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-37d0e890.js";import{w as r}from"./with-install-dbef9c21.js";const t=r(o),a=t;export{a as V};
--- vant/assets/index-ae96d7fb.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index-ec68e579.js";import{w as r}from"./with-install-dbef9c21.js";const t=r(o),a=t;export{a as V};
